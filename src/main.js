323dcdc93171727916862c83e57efc85bbc201f9
syy <869074486@qq.com>
2018-10-09 16:59:41 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
+
 import router from './router'
 import store from './store'
 import axios from './assets/axios'
@@ -10,6 +13,8 @@ import storage from './assets/utils/storage'
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.prototype.$storage = storage
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
